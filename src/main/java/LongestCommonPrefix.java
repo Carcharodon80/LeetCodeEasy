4d170c5d67b44db1179dbeae68bd0e4c2c34efc4
Carcharodon80 <karna241280@gmail.com>
2021-06-04 09:53:50 +0300

    3

---
+++ src/main/java/LongestCommonPrefix.java
@@ -0,0 +1,27 @@
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        int minWordLength = 200;
+        for (String word : strs) {
+            if (minWordLength > word.length()) {
+                minWordLength = word.length();
+            }
+        }
+        String rsl = "";
+        boolean isCommon = true;
+        for (int i = 0; i < minWordLength; i++) {
+            for (String word : strs) {
+                if (word.charAt(i) != strs[0].charAt(i)) {
+                    isCommon = false;
+                    break;
+                }
+            }
+            if (isCommon) {
+                rsl += strs[0].charAt(i);
+            } else {
+                break;
+            }
+        }
+        return rsl;
+
+    }
+}
